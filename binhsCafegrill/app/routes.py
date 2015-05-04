f1aab8d49aaab1d2c90b6effb7a3de4f543b79fa
khamichael <khamichael@hotmail.com>
2015-05-04 11:29:05 +0200

    im cool
    im cool

--- binhsCafegrill/app/routes.py
+++ binhsCafegrill/app/routes.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
- 
+
+from flask import Flask, render_template, request
+
 app = Flask(__name__)      
- 
+
+app.secret_key = 'development key'
+
 @app.route('/')
 def home():
   return render_template('home.html')
@@ -10,9 +13,6 @@ def home():
 def about():
   return render_template('about.html')
 
-@app.route('/signin')
-def signin():
-  return render_template('signin.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
