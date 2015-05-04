8f3863aabf7a8fe8449418ff4e775a4b6a13c175
khamichael <khamichael@hotmail.com>
2015-05-04 14:10:00 +0200

    mere seje ting
    jubiiii

--- binhsCafegrill/app/routes.py
+++ binhsCafegrill/app/routes.py
@@ -1,18 +1,49 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from functools import wraps
 
 app = Flask(__name__)      
 
 app.secret_key = 'development key'
+
+#login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/')
 def home():
   return render_template('home.html')
 
 @app.route('/about')
+@login_required
 def about():
   return render_template('about.html')
 
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('Du er nu logged ind')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('Du er nu logged ud')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
   app.run(debug=True)
