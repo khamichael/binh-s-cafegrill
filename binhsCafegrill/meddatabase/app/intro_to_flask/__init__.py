319fc3ecde88e8979a6e4a11cef67cc104175212
khamichael <khamichael@hotmail.com>
2015-05-07 14:41:53 +0200

    Tilføjet sign in, sign up, profile
    Tilføjet meget crap, det virker :))

--- binhsCafegrill/meddatabase/app/intro_to_flask/__init__.py
+++ binhsCafegrill/meddatabase/app/intro_to_flask/__init__.py
@@ -9,10 +9,15 @@ app.secret_key = 'development key'
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = 'contact@example.com'
-app.config["MAIL_PASSWORD"] = 'your-password'
+app.config["MAIL_USERNAME"] = 'datalogskater@gmail.com'
+app.config["MAIL_PASSWORD"] = 'datalogi'
 
 from routes import mail
 mail.init_app(app)
 
 import intro_to_flask.routes
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/development'
+
+from models import db
+db.init_app(app)
